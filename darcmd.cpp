a6a81da5d6f187605d96363a7e4f21a46cdab3dc
Sergey <kotik22157749@gmail.com>
2026-06-30 19:15:17 +0400

    Fixed glitchh with vanishing 'Access denied' message

--- darcmd.cpp
+++ darcmd.cpp
@@ -47,14 +47,21 @@ std::string input_dialog(const char* title, const char prev[128]) {
   return std::string(buf);
 }
 
-void popup_message(const char* title) {
+void popup_message(const char* title, const char* text) {
   int h = 5, w = 40;
   int y = LINES/2 - h/2, x = COLS/2 - w/2;
   WINDOW* win = newwin(h, w, y, x);
   box(win, 0, 0);
   mvwprintw(win, 0, 2, " %s ", title);
+  mvwprintw(win, 1, 1, "%s", text);
   wrefresh(win);
-
+  int ch;
+  while ((ch = getch()) != '\n')  {
+    box(win, 0, 0);
+    mvwprintw(win, 0, 2, " %s ", title);
+    mvwprintw(win, 1, 1, "%s", text);
+    wrefresh(win);
+  }
   getch();
   delwin(win);
 }
@@ -89,70 +96,56 @@ void log(const char* fmt, ...) {
   fclose(file);
 }
 
+void cd_up();
+void draw_half(int *cur, char (*fils)[64], char *pth, bool half) {
+  int start = (COLS/2) * half;
+  mvprintw(1, start, "|          File Name            |  Size  |");
+  mvprintw(0, start, "|%s", pth);
+  char name[64];
+  char entr[43];
+  memset(entr, ' ', 26);
+  entr[0 ] = '|';
+  entr[32] = '|';
+  entr[41] = '|';
+  entr[42] = '\0';
+  int j = 2, cfile = 0;
+  try {
+    for (auto& entry : fs::directory_iterator(pth, fs::directory_options::skip_permission_denied)) {
+      memset(entr, ' ', 42);
+      entr[0 ] = '|';
+      entr[42] = '\0';
+      for(int i = 0; i < 64; i++) name[i] = ' ';
+      std::string filename = utf8_to_cp1251(entry.path().filename().string());
+      snprintf(name, sizeof(name), "%s", filename.c_str());
+      for(int i = strlen(name); i < 31; i++) name[i] = ' ';
+      std::string size = entry.is_directory() ? "Folder" : std::to_string(entry.file_size());
+      memcpy(entr + 1, name, 31);
+      memcpy(fils[cfile], name, 63);
+      fils[cfile][63] = '\0';
+      entr[32] = '|';
+      memcpy(entr + 33, size.c_str(), std::min((int)size.size(), 7));  // размер без \0
+      entr[41] = '|';
+      bool sel = (cfile == *cur) && (page == half);
+      if(sel) attron(A_REVERSE);
+      mvprintw(j, start, entr);
+      if(sel) attroff(A_REVERSE);
+      j++; cfile++;
+    }
+    refresh();
+  } catch (const fs::filesystem_error& e) {
+    log("CAUGHT half=%d", half);
+    popup_message("Error", "Access denied");
+    cd_up();
+  }
+}
+
 void redraw() {
   clear();
   std::string title;
-  title = "Dar commander - " + std::string(page ? files[current] : files2[current2]);
+  title = "Dar commander - " + std::string(page ? files2[current2] : files[current]);
   SetConsoleTitleA(title.c_str());
-  mvprintw(0, 0,      "           File Name            |  Size  |");
-  mvprintw(0, COLS/2, "           File Name            |  Size  |");
-  //mvprintw(0, 0, "&cur=%p &files=%p", &current, files);
-  char name[64];
-  char entr[43];
-  memset(entr, ' ', 26);  // заполнить пробелами
-  entr[0 ] = '|';
-  entr[32] = '|';
-  entr[41] = '|';
-  entr[42] = '\0';
-  int j = 1;
-  for (auto& entry : fs::directory_iterator(path, fs::directory_options::skip_permission_denied)) {
-    memset(entr, ' ', 42);
-    entr[42] = '\0';
-    for(int i = 0; i < 64; i++) name[i] = ' ';
-    std::string filename = utf8_to_cp1251(entry.path().filename().string());
-    snprintf(name, sizeof(name), "%s", filename.c_str());
-    for(int i = strlen(name); i < 31; i++) name[i] = ' ';
-    std::string size = entry.is_directory() ? "Folder" : std::to_string(entry.file_size());
-    memcpy(entr + 1, name, 31);
-    memcpy(files[j - 1], name, 63);
-    files[j - 1][63] = '\0';
-    entr[32] = '|';
-    memcpy(entr + 33, size.c_str(), std::min((int)size.size(), 7));  // размер без \0
-    entr[41] = '|';
-    bool sel = (j - 1 == current) && !page;
-    if(sel) attron(A_REVERSE);
-    mvprintw(j, 0, entr);
-    if(sel) attroff(A_REVERSE);
-    j++;
-  }
-  refresh();
-  memset(entr, ' ', 26);  // заполнить пробелами
-  entr[0 ] = '|';
-  entr[32] = '|';
-  entr[41] = '|';
-  entr[42] = '\0';
-  j = 1;
-  for (auto& entry : fs::directory_iterator(path2, fs::directory_options::skip_permission_denied)) {
-    memset(entr, ' ', 42);
-    entr[42] = '\0';
-    for(int i = 0; i < 64; i++) name[i] = ' ';
-    std::string filename = utf8_to_cp1251(entry.path().filename().string());
-    snprintf(name, sizeof(name), "%s", filename.c_str());
-    for(int i = strlen(name); i < 31; i++) name[i] = ' ';
-    std::string size = entry.is_directory() ? "Folder" : std::to_string(entry.file_size());
-    memcpy(entr + 1, name, 31);
-    memcpy(files2[j - 1], name, 63);
-    files2[j - 1][63] = '\0';
-    entr[32] = '|';
-    memcpy(entr + 33, size.c_str(), std::min((int)size.size(), 7));  // размер без \0
-    entr[41] = '|';
-    bool sel = (j - 1 == current2) && page;
-    if(sel) attron(A_REVERSE);
-    mvprintw(j, COLS/2, entr);
-    if(sel) attroff(A_REVERSE);
-    j++;
-  }
-  refresh();
+  draw_half(&current , files , path , 0);
+  draw_half(&current2, files2, path2, 1);
 }
 
 void cd_up() {
@@ -164,6 +157,7 @@ void cd_up() {
   for (int i = 0; i < len; i++)
     if (pth[i] == '/' || pth[i] == '\\') last = i;
   if (last > 0) pth[last] = '\0';
+  if (strlen(pth) == 2 && pth[1] == ':') strcat(pth, "/");
 }
 
 int loop() {
@@ -176,7 +170,7 @@ int loop() {
   int cols = csbi.srWindow.Right - csbi.srWindow.Left + 1;
   //mvprintw(0, 0, "rows=%d cols=%d LINES=%d COLS=%d", rows, cols, LINES, COLS); refresh(); napms(2000);
   if (rows != LINES || cols != COLS) resizeterm(rows, cols);
-  redraw();
+  redraw(); // do NOT touch 
   std::string s(pth);
   std::string f = cp1251_to_utf8(std::string(fils[*cur]));
   while (!f.empty() && (f.back() == ' ' || f.back() == '\0')) f.pop_back();
@@ -185,6 +179,8 @@ int loop() {
   if (ch == '\n') return 0;
   if (ch == KEY_LEFT) {
     cd_up();
+    memset(fils, 0, sizeof(char[64][64]));
+    *cur = 0;
   }
   if (ch == KEY_RIGHT) {
     //log("KEY_RIGHT full='%s' isdir=%d", full.c_str(), fs::is_directory(full));
@@ -193,6 +189,7 @@ int loop() {
       strcpy(pth, full.c_str());
       //log("full=%s", full.c_str());
       *cur = 0;
+      memset(fils, 0, sizeof(files));
     } else {
       int wlen = MultiByteToWideChar(CP_UTF8, 0, full.c_str(), -1, nullptr, 0);
       std::wstring wfull(wlen, 0);
@@ -222,10 +219,12 @@ int loop() {
     fs::copy(full, s + (s.back() == '/' ? "" : "/") + input_dialog("Copy as:"), fs::copy_options::recursive);
   }
   if (ch == 'h') {
-    popup_message("Dar commander Alpha v0.2");
+    popup_message("Dar commander Alpha v0.2", pth);
   }
   if (ch == 'g') {
     strcpy(pth, input_dialog("Go to:", pth).c_str());
+    memset(fils, 0, sizeof(files));
+    *cur = 0;
   }
   //mvprintw(LINES-1, 0, "ch=%d cur=%d", ch, current); refresh(); napms(2000);
   redraw(); return 1;
